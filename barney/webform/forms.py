57cd428c013de39a45e400d8f271da96bd8f98d8
Mikey Reppy <mxreppy@liquidhub.com>
2015-02-09 16:57:03 -0500

    working on a form

---
+++ barney/webform/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class OrderForm(forms.Form):
+    address = forms.CharField(label='Address', max_length=256)
+    plan = forms.CharField(label='Plan Name', max_length=75)
